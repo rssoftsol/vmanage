6ca7b3b63da97dd79baa7cae4dc4b10e14dccae9
rssoftsol <otari.rahul@gmail.com>
2015-02-11 22:18:33 +0530

    New file added for logout handling

---
+++ intimator/src/main/java/com/imanage/util/NoCacheFilter.java
@@ -0,0 +1,46 @@
+package com.imanage.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class NoCacheFilter
+ */
+public class NoCacheFilter implements Filter {
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletResponse servletResponse = (HttpServletResponse) response;
+		servletResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		servletResponse.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		servletResponse.setDateHeader("Expires", 0); // Proxies.
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
